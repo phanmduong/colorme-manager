622c9a8026764d371dd4f9865d4223cd4efcab13
Dung <dxt6193@rit.edu>
2019-08-17 11:34:53 +0700

    Splash screen for android

---
+++ android/app/src/main/java/vn/colorme/phanminhduong/colormemanager/SplashActivity.java
@@ -0,0 +1,17 @@
+package vn.colorme.phanminhduong.colormemanager;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}

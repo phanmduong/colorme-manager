8673ecf961e0d54439dfbdeaec44600ad816f12f
Dung <dxt6193@rit.edu>
2019-09-03 17:41:07 +0700

    Logout

--- src/containers/DashboardContainer.js
+++ src/containers/DashboardContainer.js
@@ -3,15 +3,35 @@
  */
 import React from 'react';
 import DashboardComponent from '../components/DashboardComponent';
-import {Platform} from 'react-native';
+import {Image, Platform, TouchableOpacity} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import * as alert from '../constants/alert';
 
 class DashboardContainer extends React.Component {
   constructor(props, context) {
     super(props, context);
   }
 
+  static async clearAll(navigation) {
+    const keys = ['@ColorME:username', '@ColorME:password'];
+    try {
+      await AsyncStorage.multiRemove(keys);
+      navigation.navigate('Login');
+    } catch (error) {
+      alert(error);
+    }
+  }
+
   static navigationOptions = ({navigation}) => ({
-    headerTitle: 'Quản lý',
+    title: 'Quản lý',
+    headerRight: (
+      <TouchableOpacity onPress={() => this.clearAll(navigation)}>
+        <Image
+          source={require('../../assets/img/icons8-logout-rounded-up-100.png')}
+          style={{width: 20, height: 20, right: 20}}
+        />
+      </TouchableOpacity>
+    ),
   });
 
   render() {

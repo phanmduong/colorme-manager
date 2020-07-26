c17b91bccbf2340927c3dc355ebc50417a47d611
Dung <dxt6193@rit.edu>
2020-07-26 10:53:59 -0400

    Fix save register reload register list

--- src/containers/SaveRegisterContainer.js
+++ src/containers/SaveRegisterContainer.js
@@ -7,7 +7,7 @@ import * as registerListActions from '../actions/registerListActions';
 import * as baseActions from '../actions/baseActions';
 import {Text, View} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import theme from "../styles";
+import theme from '../styles';
 
 class SaveRegisterContainer extends React.Component {
   constructor(props, context) {
@@ -44,7 +44,7 @@ class SaveRegisterContainer extends React.Component {
     this.props.saveRegisterActions.loadCourses(this.props.token);
   };
 
-  loadClasses = courseId => {
+  loadClasses = (courseId) => {
     this.props.saveRegisterActions.loadClasses(this.props.token, courseId);
   };
 
@@ -72,16 +72,48 @@ class SaveRegisterContainer extends React.Component {
     this.props.saveRegisterActions.loadSalers(this.props.token);
   };
 
-  register = register => {
+  register = (register) => {
     this.props.saveRegisterActions.register(this.props.token, register);
   };
 
   reloadRegisterList = () => {
+    let baseId =
+      this.props.selectedBaseId === -1 ? '' : this.props.selectedBaseId;
     let salerId = this.props.salerId === -1 ? '' : this.props.salerId;
+    let campaignId = this.props.campaignId === -1 ? '' : this.props.campaignId;
+    let paidStatus = this.props.paidStatus === -1 ? '' : this.props.paidStatus;
+    let callStatus = this.props.callStatus === -1 ? '' : this.props.callStatus;
+    let bookmark = this.props.bookmark === -1 ? '' : this.props.bookmark;
+    let statusId = this.props.status_id === -1 ? '' : this.props.status_id;
+    let sourceId = this.props.source_id === -1 ? '' : this.props.source_id;
+    let genId = '';
+    if (this.props.selectedGenId === -1) {
+      genId = this.props.currentGen.id;
+    } else if (
+      this.props.selectedGenId !== -1 &&
+      this.props.selectedGenId !== 2
+    ) {
+      genId = this.props.selectedGenId;
+    }
+    let classId = this.props.classId === -1 ? '' : this.props.classId;
     this.props.registerListActions.refreshRegisterListMy(
-      '',
+      this.props.searchMy,
       this.props.token,
       salerId,
+      baseId,
+      campaignId,
+      paidStatus,
+      this.props.classStatus,
+      callStatus,
+      bookmark,
+      this.props.search_coupon,
+      this.props.start_time,
+      this.props.end_time,
+      this.props.appointmentPayment,
+      statusId,
+      sourceId,
+      genId,
+      classId,
     );
   };
 
@@ -153,6 +185,22 @@ function mapStateToProps(state) {
     isLoadingSalers: state.saveRegister.isLoadingSalers,
     errorLoadingSalers: state.saveRegister.errorLoadingSalers,
     salers: state.saveRegister.salers,
+    selectedBaseId: state.base.selectedBaseId,
+    campaignId: state.registerList.campaignId,
+    paidStatus: state.registerList.paidStatus,
+    callStatus: state.registerList.callStatus,
+    bookmark: state.registerList.bookmark,
+    status_id: state.registerList.status_id,
+    source_id: state.registerList.source_id,
+    selectedGenId: state.gen.selectedGenId,
+    currentGen: state.gen.currentGen,
+    classId: state.registerList.classId,
+    searchMy: state.registerList.searchMy,
+    classStatus: state.registerList.classStatus,
+    start_time: state.registerList.start_time,
+    end_time: state.registerList.end_time,
+    appointmentPayment: state.registerList.appointmentPayment,
+    search_coupon: state.registerList.search_coupon,
   };
 }
 

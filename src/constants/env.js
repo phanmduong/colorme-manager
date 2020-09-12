a7dfd43c85cd53d1ad21588c6f1d1d91936e0f85
Dung <dxt6193@rit.edu>
2020-09-12 16:27:41 +0700

    Analytics

--- src/constants/env.js
+++ src/constants/env.js
@@ -9,64 +9,28 @@ export const API_NODE_URL = 'http://colorme.vn:8000';
 
 export function apiUrl(domain) {
   let domainParts = domain.split('.');
-  if (domain.toLowerCase().includes('colorme')) {
-    return `http://api.${domainParts[1]}.${domainParts[2]}`;
-  } else if (domain.toLowerCase().includes('ieg')) {
-    return `http://api.${domainParts[1]}.${domainParts[2]}`;
-  } else if (domain.toLowerCase().includes('eduto')) {
-    return `http://${domainParts[0]}.${domainParts[1]}.${domainParts[2]}/api/v3`;
-  }
+  return `http://${domainParts[0]}.eduto.net/api/v3`;
 }
 
 export function manageApiUrl(domain) {
   let domainParts = domain.split('.');
-  if (domain.toLowerCase().includes('colorme')) {
-    return `http://manageapi.${domainParts[1]}.${domainParts[2]}`;
-  } else if (domain.toLowerCase().includes('ieg')) {
-    return `http://manageapi.${domainParts[1]}.${domainParts[2]}`;
-  } else if (domain.toLowerCase().includes('eduto')) {
-    return `http://${domainParts[0]}.${domainParts[1]}.${domainParts[2]}/manageapi/v3`;
-  }
+  return `http://${domainParts[0]}.eduto.net/manageapi/v3`;
 }
 
 export function baseUrl(domain) {
   let domainParts = domain.split('.');
-  if (domain.toLowerCase().includes('colorme')) {
-    return `https://${domainParts[1]}.${domainParts[2]}`;
-  } else if (domain.toLowerCase().includes('ieg')) {
-    return `https://${domainParts[1]}.${domainParts[2]}`;
-  } else if (domain.toLowerCase().includes('eduto')) {
-    return `http://${domainParts[0]}.${domainParts[1]}.${domainParts[2]}`;
-  }
+  return `http://${domainParts[0]}.eduto.net`;
 }
 
 export function manageApiUrlV3(domain) {
-  if (domain.toLowerCase().includes('colorme')) {
-    return baseUrl(domain) + '/manageapi/v3';
-  } else if (domain.toLowerCase().includes('ieg')) {
-    return baseUrl(domain) + '/manageapi/v3';
-  } else if (domain.toLowerCase().includes('eduto')) {
-    return baseUrl(domain) + '/manageapi/v3';
-  }
+  return baseUrl(domain) + '/manageapi/v3';
 }
 
 export function apiNodeUrl(domain) {
   let domainParts = domain.split('.');
-  if (domain.toLowerCase().includes('colorme')) {
-    return `http://${domainParts[1]}.${domainParts[2]}:8000`;
-  } else if (domain.toLowerCase().includes('ieg')) {
-    return `http://${domainParts[1]}.${domainParts[2]}:8000`;
-  } else if (domain.toLowerCase().includes('eduto')) {
-    return `http://${domainParts[0]}.${domainParts[1]}.${domainParts[2]}:8000`;
-  }
+  return `http://${domainParts[0]}.eduto.net:8000`;
 }
 
 export function manageApiUrlV4(domain) {
-  if (domain.toLowerCase().includes('colorme')) {
-    return baseUrl(domain) + '/manageapi/v4';
-  } else if (domain.toLowerCase().includes('ieg')) {
-    return baseUrl(domain) + '/manageapi/v4';
-  } else if (domain.toLowerCase().includes('eduto')) {
-    return baseUrl(domain) + '/manageapi/v4';
-  }
+  return baseUrl(domain) + '/manageapi/v4';
 }

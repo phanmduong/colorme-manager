49a28152baffe073bf1b5ced9ef3fc3785c11377
Dung <dxt6193@rit.edu>
2020-12-19 15:19:11 +0700

    Add, edit, delete, duplicate, view, search course lessons.

--- src/components/AddCourseLessonComponent.js
+++ src/components/AddCourseLessonComponent.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import {
   View,
   ScrollView,
@@ -14,14 +14,26 @@ import InputPicker from './common/InputPicker';
 
 function AddCourseLessonComponent(props) {
   const [name, setName] = useState(props.editMode && props.lesson.name);
-  const [order, setOrder] = useState(props.editMode && props.lesson.order.toString());
-  const [description, setDescription] = useState(props.editMode && props.lesson.description);
+  const [order, setOrder] = useState(
+    props.editMode && props.lesson.order.toString(),
+  );
+  const [description, setDescription] = useState(
+    props.editMode && props.lesson.description,
+  );
   const [isExpanded, setExpanded] = useState(false);
-  const [googleLink, setGoogleLink] = useState(props.editMode && props.lesson.detail_teacher);
-  const [audioLink, setAudioLink] = useState(props.editMode && props.lesson.audio_url);
-  const [videoLink, setVideoLink] = useState(props.editMode && props.lesson.video_url);
+  const [googleLink, setGoogleLink] = useState(
+    props.editMode && props.lesson.detail_teacher,
+  );
+  const [audioLink, setAudioLink] = useState(
+    props.editMode && props.lesson.audio_url,
+  );
+  const [videoLink, setVideoLink] = useState(
+    props.editMode && props.lesson.video_url,
+  );
   const [termId, setTerm] = useState(props.editMode && props.lesson.term_id);
-  const [id, setId] = useState(props.editMode && props.lesson.id);
+
+  const audioLinkRef = useRef(null);
+  const videoLinkRef = useRef(null);
 
   function toggleExpand() {
     setExpanded(!isExpanded);
@@ -51,7 +63,7 @@ function AddCourseLessonComponent(props) {
           order: order,
           video_url: videoLink,
           term_id: termId,
-          id: id,
+          id: props.lesson.id,
         };
         props.editLesson(data);
       }
@@ -104,18 +116,23 @@ function AddCourseLessonComponent(props) {
                 placeholder={'Link Google Slide'}
                 onChangeText={setGoogleLink}
                 value={googleLink}
+                onSubmitEditing={() => audioLinkRef.current.focus()}
               />
               <Input
                 title={'Link audio'}
                 placeholder={'Link audio'}
                 onChangeText={setAudioLink}
                 value={audioLink}
+                refName={audioLinkRef}
+                onSubmitEditing={() => videoLinkRef.current.focus()}
               />
               <Input
                 title={'Link video'}
                 placeholder={'Link video'}
                 onChangeText={setVideoLink}
                 value={videoLink}
+                refName={videoLinkRef}
+                onSubmitEditing={() => videoLinkRef.current.focus()}
               />
             </>
           )}

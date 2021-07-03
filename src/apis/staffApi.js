248f5bb89982f274bac36d93bd190df71504c05b
Dung <dxt6193@rit.edu>
2021-07-03 18:22:14 -0400

    Staff clean up

--- src/apis/staffApi.js
+++ src/apis/staffApi.js
@@ -12,16 +12,3 @@ export function getStaff(sourceCancel = {}, page, search, token, domain) {
     token;
   return axios.get(url, {cancelToken: sourceCancel.token});
 }
-
-export function getDepartments(token, domain) {
-  let url =
-    env.manageApiUrlV3(domain) +
-    '/department/get-all-departments?limit=-1&token=' +
-    token;
-  return axios.get(url);
-}
-
-export function getRoles(token, domain) {
-  let url = env.manageApiUrlV3(domain) + '/get-roles?token=' + token;
-  return axios.get(url);
-}
